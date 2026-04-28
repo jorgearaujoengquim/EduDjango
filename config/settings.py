a1adb7f7ccf97379e1f8c40f8d5d6c0da9324355
jorgearaujoengquim <jorgearaujoengquim@gmail.com>
2026-04-28 10:56:40 -0300

    Modify Django settings for production deployment
    Updated settings for production environment, including disabling debug mode and allowing all hosts.

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dif!+v+=bectho$x)dbsu#sb*#^z=$qr*848ckh$80vu00tdpb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*'] # Mudar depois que tiver o dominio
 
 
 # Application definition
@@ -116,3 +116,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR/'staticfiles'
